04002589dcab1280c1782e3aa043b2bd390862cf
juniorcostadev157 <juniorcosta15785@gmail.com>
2025-10-11 00:20:09 -0300

    tela completa de login  mvvvm clean +injecao manual

---
+++ app/src/main/java/com/junior/projetomvvmcleanxml/data/model/UserResponse.kt
@@ -0,0 +1,7 @@
+package com.junior.projetomvvmcleanxml.data.model
+
+data class UserResponse (
+    val id: String= "",
+    val email: String? = "",
+    val nome: String? = ""
+)
---
+++ app/src/main/java/com/junior/projetomvvmcleanxml/domain/usecase/authenticationusecase/LoginValidationUseCase.kt
@@ -0,0 +1,31 @@
+package com.junior.projetomvvmcleanxml.domain.usecase.authenticationusecase
+
+import com.junior.projetomvvmcleanxml.data.repository.authrepository.AuthError
+import com.junior.projetomvvmcleanxml.domain.repository.AuthRepository
+
+class LoginValidationUseCase(
+    private val repository: AuthRepository
+) {
+    suspend operator fun invoke(email: String , password: String): ValidationResult{
+
+        if (email.isBlank() || !email.contains("@")){
+            return ValidationResult(false, "Email Invalido")
+        }
+
+        if (password.isBlank()){
+            return ValidationResult(false, "Preencha o Senha")
+        }
+
+        val result = repository.login(email, password)
+        return if (result.isSuccess){
+            ValidationResult(true)
+        }else{
+                val exception = result.exceptionOrNull()
+            println("🔥 Tipo do erro: ${exception?.javaClass?.name}")
+            println("🔥 Mensagem do erro: ${exception?.message}")
+            ValidationResult(false, (result.exceptionOrNull() as? AuthError)?.messageError)
+        }
+        }
+
+
+    }
